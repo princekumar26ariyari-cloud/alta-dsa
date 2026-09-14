fd4f12c778bd8f83514f499ebca16a36e4447b53
princekumar26ariyari-cloud <princekumar26ariyari@gmail.com>
2026-09-14 11:46:16 +0530

    Add files via upload

---
+++ prince.cpp
@@ -0,0 +1,16 @@
+Make Plain Text
+#include <iostream>
+using namespace std;
+
+int main() {
+    int days=7305;
+    cout <<"Enter your days";
+    cin>>days;
+
+    int years=days/365;
+    int remdays=days%365;
+
+    cout<<years<<"years,";
+    cout<<days<<"days";
+    return 0;
+}
