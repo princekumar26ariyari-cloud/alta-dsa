277e675fab025b6a87f38d5e9e7974d06a82fe29
prince kumar <princekumar26ariyari@gmail.com>
2026-09-22 15:59:24 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,19 @@
+ #include <iostream>
+ using namespace std;
+
+ int main(){
+int days, years, remDays;
+
+cout<<"Enter days";
+
+cin>>days;
+
+years=days/365;
+
+remDays=days%365;
+
+cout<<years<<"years"<<remDays<<"days";
+
+return 0;
+ }
+
